ddd9c214c6bcdc7e6c28e1ee590f4eea8ec0b41f
vaddera <vaddera@rose-hulman.edu>
2014-10-28 03:35:16 -0400

    algorithms.py running BM25
    Well, idk if the results are right though... lol

---
+++ algorithms.py
@@ -0,0 +1,88 @@
+'''
+Created on Oct 27, 2014
+
+@author: vaddera
+'''
+'''---------------------------------------------------------------
+    Imports:
+------------------------------------------------------------------'''
+from readFiles import readFile
+import math
+'''---------------------------------------------------------------
+    Variable initialization
+------------------------------------------------------------------'''
+
+keywords = ['adams','lincoln','president','assassinated president','great president','first president','civil war president','second president','impeached','general','world war',
+            'founding','democrat','republican','independent','congress','law']
+textFiles = ['docs\AbrahamLincoln.txt','docs\AndrewJackson.txt','docs\AndrewJohnson.txt','docs\BarackObama.txt','docs\BenjaminHarrison.txt','docs\BillClinton.txt',
+             'docs\CalvinCoolidge.txt','docs\ChesterArthur.txt','docs\DwightEisenhower.txt','docs\FranklinDRoosevelt.txt','docs\FranklinPierce.txt','docs\GeorgeBush.txt',
+             'docs\GeorgeWashington.txt','docs\GeorgeWBush.txt','docs\GeraldFord.txt','docs\GroverCleveland.txt','docs\HarryTruman.txt','docs\HerbertHoover.txt',
+             'docs\JamesBuchanan.txt','docs\JamesGarfield.txt','docs\JamesMadison.txt','docs\JamesMonroe.txt','docs\JamesPolk.txt','docs\JimmyCarter.txt',
+             'docs\JohnAdams.txt','docs\JohnKennedy.txt','docs\JohnQuincyAdams.txt','docs\JohnTyler.txt','docs\LyndonJohnson.txt','docs\MartinVanBuren.txt','docs\MillardFillmore.txt',
+             'docs\RichardNixon.txt','docs\RonaldReagan.txt','docs\RutherfordHayes.txt','docs\TheodoreRoosevelt.txt','docs\ThomasJefferson.txt','docs\UlyssesGrant.txt',
+             'docs\WarrenHarding.txt','docs\WilliamHenryHarrison.txt','docs\WilliamMcKinley.txt','docs\WilliamTaft.txt','docs\WoodrowWilson.txt','docs\ZacharyTaylor.txt']
+
+''' BM25 variables: '''
+b = 0.75
+k1 = 1.2 # can range from 1.2 to 2.0
+N = len(textFiles)
+
+'''----------------------------------------------------------------
+    Algorithms:
+-------------------------------------------------------------------'''
+
+def BM25():
+    contain = 0
+    prevContain = 0
+    n = 0
+    lengthDocs = []
+    idfList = []
+    scores = []
+    preScore = []
+    
+    for i in range(len(keywords)):
+        for j in range(len(textFiles)):
+            lenD, freq = readFile(textFiles[j], keywords[i])
+            contain = contain + freq
+            
+            if contain != prevContain:
+                n = n + 1
+            
+            prevContain = contain
+            
+            if i==0:
+                lengthDocs.append(lenD)
+        
+        # Calculating the IDF:    
+        tmp = (N - n + 0.5)/(n + 0.5)
+        idf = math.log(abs(tmp))
+        idfList.append(idf)
+        
+        # Clearing the accumulated values from the variables:
+        contain = 0
+        n = 0
+    
+    # Calculating avgdl:
+    avgdl = sum(lengthDocs)/N # Makes no sense why there is an error here...
+    
+    for i in range(len(textFiles)):
+        for j in range(len(keywords)):
+            
+            lengthD, frequency = readFile(textFiles[i], keywords[j])
+            
+            tmpScore = idfList[j]*((frequency * (k1 + 1))/(frequency + k1*(1 - b + (b*lengthD/avgdl))))
+            
+            preScore.append(tmpScore)
+        
+        score = sum(preScore) # sum all the calculated scores for the current document
+        scores.append(score) # put each calculated score for each document in an array.
+
+        
+    print scores
+    
+'''---------------------------------------------------------------
+    Running algorithms:
+------------------------------------------------------------------'''
+    
+BM25()
+    
--- readFiles.py
+++ readFiles.py
@@ -5,9 +5,10 @@ Created on Oct 27, 2014
 '''
 import collections
 import re
-#import sys
-
+'''
 count = 0
+prevCount = 0
+foundInDocument = 0
 
 textFiles = ['docs\AbrahamLincoln.txt','docs\AndrewJackson.txt','docs\AndrewJohnson.txt','docs\BarackObama.txt','docs\BenjaminHarrison.txt','docs\BillClinton.txt',
              'docs\CalvinCoolidge.txt','docs\ChesterArthur.txt','docs\DwightEisenhower.txt','docs\FranklinDRoosevelt.txt','docs\FranklinPierce.txt','docs\GeorgeBush.txt',
@@ -18,26 +19,39 @@ textFiles = ['docs\AbrahamLincoln.txt','docs\AndrewJackson.txt','docs\AndrewJohn
              'docs\WarrenHarding.txt','docs\WilliamHenryHarrison.txt','docs\WilliamMcKinley.txt','docs\WilliamTaft.txt','docs\WoodrowWilson.txt','docs\ZacharyTaylor.txt']
 
 lengthDocs = []
+'''
 
 def readFile(file_input, matchingWord):
+    #termsCount = 0
     
     with open(file_input,'r') as content_file:
         content = content_file.read()
         words = re.split("\W+",content.lower())
+        #if matchingWord in content:
+            #termsCount = termsCount + 1
 
     wordsCount = len(words)        
     termsCount = collections.Counter(words)
 
     return (int(wordsCount),int(termsCount[matchingWord]))
+    #return (int(wordsCount),termsCount)
 
 '''---------------------------------------------------------------------
     Here the function is called and tested for each of the documents:
 ------------------------------------------------------------------------'''
-
+'''
 for i in range(len(textFiles)):
     lenDoc, newCount = readFile(textFiles[i],'the')
     lengthDocs.append(lenDoc)
     count = count + newCount
+    
+    if count != prevCount:
+        foundInDocument = foundInDocument + 1
+    
+    prevCount = count
     print 'Length of the ' + str(i + 1) + ' document is ' + str(lengthDocs[i])
+    print newCount
     
 print 'Number of the counted specified words: ' + str(count)
+print 'Keyword was found in ' + str(foundInDocument) + ' documents.'
+'''
