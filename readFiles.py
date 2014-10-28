d81562f5bee46f9d282ff76031937ffa742aee89
vaddera <vaddera@rose-hulman.edu>
2014-10-27 20:30:12 -0400

    readFiles.py updated

--- readFiles.py
+++ readFiles.py
@@ -5,37 +5,34 @@ Created on Oct 27, 2014
 '''
 import collections
 import re
-import sys
+#import sys
 
-def readFile(matchingWord):
-    textFiles = ['docs\AbrahamLincoln.txt','docs\AndrewJackson.txt','docs\AndrewJohnson.txt','docs\BarackObama.txt','docs\BenjaminHarrison.txt','docs\BillClinton.txt',
-                 'docs\CalvinCoolidge.txt','docs\ChesterArthur.txt','docs\DwightEisenhower.txt','docs\FranklinDRoosevelt.txt','docs\FranklinPierce.txt','docs\GeorgeBush.txt',
-                 'docs\GeorgeWashington.txt','docs\GeorgeWBush.txt','docs\GeraldFord.txt','docs\GroverCleveland.txt','docs\HarryTruman.txt','docs\HerbertHoover.txt',
-                 'docs\JamesBuchanan.txt','docs\JamesGarfield.txt','docs\JamesMadison.txt','docs\JamesMonroe.txt','docs\JamesPolk.txt','docs\JimmyCarter.txt',
-                 'docs\JohnAdams.txt','docs\JohnKennedy.txt','docs\JohnQuincyAdams.txt','docs\JohnTyler.txt','docs\LyndonJohnson.txt','docs\MartinVanBuren.txt','docs\MillardFillmore.txt',
-                 'docs\RichardNixon.txt','docs\RonaldReagan.txt','docs\RutherfordHayes.txt','docs\TheodoreRoosevelt.txt','docs\ThomasJefferson.txt','docs\UlyssesGrant.txt',
-                 'docs\WarrenHarding.txt','docs\WilliamHenryHarrison.txt','docs\WilliamMcKinley.txt','docs\WilliamTaft.txt','docs\WoodrowWilson.txt','docs\ZacharyTaylor.txt']
+count = 0
 
-    count = 0
+textFiles = ['docs\AbrahamLincoln.txt','docs\AndrewJackson.txt','docs\AndrewJohnson.txt','docs\BarackObama.txt','docs\BenjaminHarrison.txt','docs\BillClinton.txt',
+             'docs\CalvinCoolidge.txt','docs\ChesterArthur.txt','docs\DwightEisenhower.txt','docs\FranklinDRoosevelt.txt','docs\FranklinPierce.txt','docs\GeorgeBush.txt',
+             'docs\GeorgeWashington.txt','docs\GeorgeWBush.txt','docs\GeraldFord.txt','docs\GroverCleveland.txt','docs\HarryTruman.txt','docs\HerbertHoover.txt',
+             'docs\JamesBuchanan.txt','docs\JamesGarfield.txt','docs\JamesMadison.txt','docs\JamesMonroe.txt','docs\JamesPolk.txt','docs\JimmyCarter.txt',
+             'docs\JohnAdams.txt','docs\JohnKennedy.txt','docs\JohnQuincyAdams.txt','docs\JohnTyler.txt','docs\LyndonJohnson.txt','docs\MartinVanBuren.txt','docs\MillardFillmore.txt',
+             'docs\RichardNixon.txt','docs\RonaldReagan.txt','docs\RutherfordHayes.txt','docs\TheodoreRoosevelt.txt','docs\ThomasJefferson.txt','docs\UlyssesGrant.txt',
+             'docs\WarrenHarding.txt','docs\WilliamHenryHarrison.txt','docs\WilliamMcKinley.txt','docs\WilliamTaft.txt','docs\WoodrowWilson.txt','docs\ZacharyTaylor.txt']
 
-    for i in range(len(textFiles)):
+def readFile(file_input, matchingWord):
     
-        with open(textFiles[i],'r') as content_file:
-            content = content_file.read()
-            lineCount = len(re.split("\n",content))
-            words = re.split("\W+",content.lower())
+    with open(file_input,'r') as content_file:
+        content = content_file.read()
+        words = re.split("\W+",content.lower())
         
-        words_count = collections.Counter(words)
-        count = count + int(words_count[matchingWord])
+    words_count = collections.Counter(words)
 
-    # Printing the number of counted words for debugging, comment if necessary.
-    print 'Number of the counted specified words: ' + str(count)
+    return int(words_count[matchingWord])
+
+'''---------------------------------------------------------------------
+    Here the function is called and tested for each of the documents:
+------------------------------------------------------------------------'''
+
+for i in range(len(textFiles)):
+    newCount = readFile(textFiles[i],'the')
+    count = count + newCount
     
-    return count
-
-'''---------------------------------------------------------
-            Here the function is called and tested:
-------------------------------------------------------------'''
-
-# Pass any string as an input parameter.
-readFile('the')
+print 'Number of the counted specified words: ' + str(count)
